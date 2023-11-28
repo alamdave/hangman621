2acbb4211f85062531de405a96cd92138d4432a8
Alamdave Gill <alamdave@gmail.com>
2023-11-28 08:44:28 +0000

    Checking the guess is correct or incorrect feature

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,8 +1,14 @@
-while True:
+word = "apple"
+
+while True:    
     print("Enter a Letter:")
-    guess = input()
+    guess = input().lower()
     if len(guess) == 1 and guess.isalpha():
-        print("Good Guess!")
+        if(guess in word):
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
         break
     else:
         print("Oops! That is not a valid input.")
+
