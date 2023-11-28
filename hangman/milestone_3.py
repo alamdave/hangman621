20c7c44a1c3ee482b60730e0466adc85e92bb8f9
Alamdave Gill <alamdave@gmail.com>
2023-11-28 08:50:47 +0000

    Improved readability and decreased repeated code with the use of functions

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,14 +1,19 @@
 word = "apple"
-
-while True:    
-    print("Enter a Letter:")
-    guess = input().lower()
-    if len(guess) == 1 and guess.isalpha():
-        if(guess in word):
-            print(f"Good guess! {guess} is in the word.")
+def ask_for_input():
+    while True:    
+        print("Enter a Letter:")
+        guess = input().lower()
+        if len(guess) == 1 and guess.isalpha():
+            break
         else:
-            print(f"Sorry, {guess} is not in the word. Try again.")
-        break
-    else:
-        print("Oops! That is not a valid input.")
+            print("Oops! That is not a valid input.")
+    check_guess(guess)
 
+def check_guess(guess):
+    if(guess in word):
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+ask_for_input()
